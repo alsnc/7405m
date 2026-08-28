2ecc2d88b671c892d94fbd7aabd4c25f5f7a2bee
harryd522 <shirllen.tong@gmail.com>
2026-08-29 02:29:59 +0800

    Update lift.cpp
    Lift Macro turns the lift motors to a certain angle, proportional control.
    Also declared in lift.hpp

--- include/lift.h
+++ include/lift.h
@@ -1,3 +1,3 @@
 #pragma once
 void lift(int lift_voltage);
-
+void liftMacro(double macroAngle, double lift_voltage,double kP,int toleranceInDegrees);
--- src/lift.cpp
+++ src/lift.cpp
@@ -1,5 +1,18 @@
 #include "main.h"
-#include "pros/motors.h"
+#include <cmath>
+#include <cstdlib>
+double error;
+
 void lift(int lift_voltage){
     lift_motors.move_voltage(lift_voltage);
+}
+
+void liftMacro(double macroAngle, double lift_voltage,double kP,int toleranceInDegrees){
+    error=lift_motors.get_position()-macroAngle;
+    while(fabs(error)<toleranceInDegrees){
+        error=lift_motors.get_position()-macroAngle;
+        lift_motors.move_voltage(lift_voltage*error*kP+120); 
+        /* Be careful when filling the values because the voltage is measured in milivolts. 
+        The voltage range is from 12000 to -12000. */
+    }
 }
