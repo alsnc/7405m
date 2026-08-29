1bb7ed8852e2b65f14dd2f49d703c4e9c7964173
harryd522 <shirllen.tong@gmail.com>
2026-08-30 07:43:42 +0800

    Update lift.cpp
    KD controller

--- src/lift.cpp
+++ src/lift.cpp
@@ -7,12 +7,17 @@ void lift(int lift_voltage){
     lift_motors.move_voltage(lift_voltage);
 }
 
-void liftMacro(double macroAngle, double lift_voltage,double kP,int toleranceInDegrees){
+void liftMacro(double macroAngle, double lift_voltage,double kP,double kD,int toleranceInDegrees){
     error=lift_motors.get_position()-macroAngle;
+    double last_time=error;
+    double this_time;
     while(fabs(error)>toleranceInDegrees){
         error=lift_motors.get_position()-macroAngle;
-        lift_motors.move_voltage(lift_voltage*error*kP+120); 
+        double this_time=error;
+        double de=this_time-last_time;
+        lift_motors.move_voltage(lift_voltage*(error*kP+kD*de/0.02));
         /* Be careful when filling the values because the voltage is measured in milivolts. 
         The voltage range is from 12000 to -12000. */
+        double last_time=this_time;
     }
 }
